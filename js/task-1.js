125652dd9cdfbadbc9b44c5cbc59df6380b8bfa5
VolodymyrV <kotsiubkov@gmail.com>
2025-08-06 17:27:18 +0300

    one

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,28 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+function getCategories() {
+  return document.querySelectorAll("#categories .item");
+}
+
+function countCategories() {
+    return getCategories().length;
+}
+
+function getTitleName(category) {
+    return category.querySelector("h2").textContent
+}
+
+function getTitleLength(category) {
+    return category.querySelectorAll("ul li").length;
+}
+
+function infoCategories() {
+    console.log(`Number of categoreis: ${countCategories()}`)
+
+    getCategories().forEach(category => {
+        console.log(`Category: ${getTitleName(category)}`);
+        console.log(`Elements: ${getTitleLength(category)}`);
+    }) 
+}
+
+
+infoCategories()
--- js/task-2.js
+++ js/task-2.js
@@ -1,30 +1,85 @@
-class Storage {
-  #items;
-  constructor(parameters) {
-    this.#items = parameters;
-  }
-  getItems() {
-    return this.#items;
-  }
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-  removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
-  }
+// МЕТОД 1 простіший
+
+// function addElem(element) {
+//   const linkGal = document.querySelector(".gallery");
+//   const lastItem = document.createElement("li");
+//   const createImg = document.createElement("img");
+//   createImg.src = element.url;
+//   createImg.alt = element.alt;
+//   lastItem.appendChild(createImg)
+//   linkGal.append(lastItem);
+   
+// }
+// function doingGalery(images) {
+//   images.forEach(element => {
+//     addElem(element)
+//   });
+// }
+
+
+// МЕТОД 2 складніший, переробив на фунції, бо після 1 методу вирішення розумів,
+// що можна таке ж зробити через функії, тому з допомогою чату написав метод 2
+
+function createImage({ url, alt }) {
+  const createImg = document.createElement("img");
+  createImg.classList.add("img")
+  createImg.src = url;
+  createImg.alt = alt;
+  return createImg;
 }
 
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem('Droid');
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+function createListItem(imageInf) {
+  const lastItem = document.createElement("li");
+  lastItem.appendChild(createImage(imageInf));
+  return lastItem;
+}
 
-storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+function addElem(element) {
+  const linkGal = document.querySelector(".gallery");
+  const li = createListItem(element);
+  linkGal.appendChild(li);
+  
+}
 
-storage.removeItem('Scaner');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+function doingGalery(images) {
+  images.forEach(element => addElem(element));
+}
+
+
+
+
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  }
+];
+
+
+
+
+
+
+doingGalery(images)
