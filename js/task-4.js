881ef11141a7b86fff867c3179030653976fba7d
VolodymyrV <kotsiubkov@gmail.com>
2025-08-07 11:50:41 +0300

    fourth done

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,34 @@
-const linkForm = document.querySelector('.login-form');
+const registerForm = document.querySelector(".login-form");
+const formMessage = document.querySelector(".form-message");
 
-linkForm.addEventListener('submit', handleSubmit);
+registerForm.addEventListener("submit", handleSubmit);
 
-function handleSubmit() {}
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const login = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    const userData = {
+        email: login,
+        password
+    };
+
+    if (!login || !password) {
+        showMessage("You need to fill in all form fields!", "red");
+        console.log("All form fields must be filled in");
+        return; 
+    } 
+
+    console.log(userData);
+    showMessage("");
+    form.reset(); 
+    
+}
+
+function showMessage(text, color = "black") {
+        formMessage.textContent = text;
+        formMessage.style.color = color;
+    }
+
+
+
